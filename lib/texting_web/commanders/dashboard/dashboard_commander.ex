03ba25647d3893b23a602c8963483c778d8b4f6a
Taehwan Kim <wlminimal@gmail.com>
2018-11-09 11:23:39 -0800

    bug fix in dashboard_commander

--- lib/texting_web/commanders/dashboard/dashboard_commander.ex
+++ lib/texting_web/commanders/dashboard/dashboard_commander.ex
@@ -8,17 +8,31 @@ defmodule TextingWeb.Dashboard.DashboardCommander do
     IO.inspect(order_id)
 
     user_id = socket.assigns.current_user_id
-    # order = Sales.get_order_by_id(order_id, user_id)
-    # message_status = Messenger.get_all_message_status(order_id)
+    IO.puts("++++ get_order_by_id ++++")
+    order = Sales.get_order_by_id(order_id, user_id)
 
-    # total_sent = Enum.count(message_status)
-    # deilvered_count = Analytics.get_delivered_message_status_count(message_status)
-    # undelivered_count = Analytics.get_undelivered_message_status_count(message_status)
-    # bitly = Bitly.get_bitly_by_order_id(order_id)
-    # total_clicks = Analytics.load_clicks(bitly)
+    IO.puts("++++ get_all_message_status ++++")
+    message_status = Messenger.get_all_message_status(order_id)
 
-    {total_sent, deilvered_count, undelivered_count, total_clicks} =
-      Analytics.get_total_analytics(user_id, order_id)
+    IO.puts("++++ Enum.count(message_status) ++++")
+    total_sent = Enum.count(message_status)
+
+    IO.puts("++++ get_delivered_message_status_count ++++")
+    deilvered_count = Analytics.get_delivered_message_status_count(message_status)
+
+    IO.puts("++++ get_undelivered_message_status_count ++++")
+    undelivered_count = Analytics.get_undelivered_message_status_count(message_status)
+
+    IO.puts("++++ get_bitly_by_order_id ++++")
+    bitly = Bitly.get_bitly_by_order_id(order_id)
+
+    IO.puts("++++ load_clicks ++++")
+    total_clicks = Analytics.load_clicks(bitly)
+
+    # {total_sent, deilvered_count, undelivered_count, total_clicks} =
+    #   Analytics.get_total_analytics(user_id, order_id)
+
+    IO.puts("++++ poking ++++")
 
     poke(socket,
       total_sent: total_sent,
@@ -28,6 +42,7 @@ defmodule TextingWeb.Dashboard.DashboardCommander do
     )
 
     # recent_order: order
+    IO.puts("++++ update charts ++++")
     socket |> exec_js!("DashboardChart.update()")
   end
 end
