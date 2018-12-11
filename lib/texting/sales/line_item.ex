40ac6b742e78a49d4b9759e0d472c1e79bbe32ff
Taehwan Kim <wlminimal@gmail.com>
2018-12-11 09:53:55 -0800

    Remove required name for adding recipients

--- lib/texting/sales/line_item.ex
+++ lib/texting/sales/line_item.ex
@@ -4,14 +4,14 @@ defmodule Texting.Sales.LineItem do
   alias Texting.Sales.LineItem
 
   embedded_schema do
-    field :person_id, :integer
-    field :name, :string
-    field :phone_number, :string
+    field(:person_id, :integer)
+    field(:name, :string)
+    field(:phone_number, :string)
   end
 
   def changeset(%LineItem{} = line_item, attrs) do
     line_item
     |> cast(attrs, [:person_id, :name, :phone_number])
-    |> validate_required([:person_id, :name, :phone_number])
+    |> validate_required([:person_id, :phone_number])
   end
 end
